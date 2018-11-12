70ac582244fabc99d179ada4a2041dc4f6361585
Justine Krejcha <justine@justinekrejcha.com>
2018-11-11 17:32:04 -0800

    Implement new config file feature.

--- DoorSign/Program.cs
+++ DoorSign/Program.cs
@@ -1,38 +1,64 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 
 namespace DoorSign
 {
 	class Program
 	{
 		private static MinecraftServer Server { get; set; }
+		private static Logger Log { get; set; }
+		private static Settings Config { get; set; }
 
 		static void Main(string[] args)
 		{
-			Settings config = new Settings();
-			Logger logger = new Logger(Console.Out);
+			Log = new Logger(Console.Out);
 #if DEBUG
-			logger.MinLevel = LogLevel.Debug;
+			Log.MinLevel = LogLevel.Debug;
 #endif
-			config.KickMessage = "The server is not online right now...";
-			config.MessageOfTheDay = new MessageOfTheDay()
+			try
 			{
-				description = "§3The server is not online at the moment\n(Although DoorSign is...)",
-				players = new PlayerCount()
-				{
-					max = 0,
-					online = 0,
-				},
-				version = new ServerVersion()
-				{
-					name = "DoorSign 1.13.2",
-					protocol = 404,
-				},
-			};
-			config.Port = 25565;
-			Server = new MinecraftServer(config, new Logger(Console.Out, LogLevel.Info));
+				String configStr = File.ReadAllText("config.cfg");
+				Config = JsonConvert.DeserializeObject<Settings>(configStr);
+			}
+			catch (IOException iex) when (iex is FileNotFoundException)
+			{
+				Log.Warning("Could not find configuration file; creating a new one");
+				Config = new Settings();
+				WriteDefaultConfig();
+			}
+			catch (UnauthorizedAccessException)
+			{
+				Log.Warning("Application does not have permission to access the configuration file; using default one");
+				Config = new Settings();
+			}
+			catch (Exception ex)
+			{
+				Log.Warning("Error occurred while loading configuration file");
+				Log.Warning(ex.ToString());
+			}
+			Server = new MinecraftServer(Config, Log);
 			Console.CancelKeyPress += Console_CancelKeyPress;
 			Server.Start();
-			while (true) { System.Threading.Thread.Sleep(1000); }
+			while (Server.Running) { System.Threading.Thread.Sleep(1000); }
+		}
+
+		private static void WriteDefaultConfig()
+		{
+			try
+			{
+				File.WriteAllText("config.cfg", JsonConvert.SerializeObject(Config, Formatting.Indented));
+			}
+			catch (IOException iex)
+			{
+				Log.Error("Could not save default configuration.");
+				Log.Error(iex.ToString());
+			}
+			catch (UnauthorizedAccessException)
+			{
+				Log.Warning("Application does not have permission to access the configuration file; using default one");
+				Config = new Settings();
+			}
 		}
 
 		private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
