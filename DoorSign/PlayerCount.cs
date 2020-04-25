faad2d06d0926169eec543c45f3c22acaf12d7b2
Justin Krejcha <justin@justinkrejcha.com>
2020-04-25 02:05:10 -0700

    Actually migrate to .NET Core

--- DoorSign/PlayerCount.cs
+++ DoorSign/PlayerCount.cs
@@ -7,10 +7,19 @@ using System.Threading.Tasks;
 
 namespace DoorSign
 {
+	/// <summary>
+	/// The player count.
+	/// </summary>
 	[JsonObject]
 	public class PlayerCount
 	{
+		/// <summary>
+		/// Maximum amount of users this server supports.
+		/// </summary>
 		public int max;
+		/// <summary>
+		/// The amount of users currently online.
+		/// </summary>
 		public int online;
 	}
 }
